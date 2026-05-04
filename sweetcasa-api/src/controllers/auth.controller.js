90b0ad2eb9784a87f58560e9b16a4f2f5c2ebc11
DevNdehPaul <paulzuhwaimbohndeh@gmail.com>
2026-05-04 09:45:26 +0100

    Refactor PrismaClient initialization in auth.controller
    Remove redundant PrismaClient initialization.

--- sweetcasa-api/src/controllers/auth.controller.js
+++ sweetcasa-api/src/controllers/auth.controller.js
@@ -13,8 +13,6 @@ function getPrisma() {
   }
   return prisma
 }
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-const prisma = new PrismaClient({ adapter })
 
 const signToken = (user) =>
   jwt.sign(
